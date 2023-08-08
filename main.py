b995d24c0572a22a355c6ad578ed84f30d86a511
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-08-08 12:33:07 +0200

    extract text from all pages

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ file_path = r"C:\Users\zbign_x5x2ftd\OneDrive\Dokumenty\Overcoming_the_Five_Dysf
 
 with open(file_path, 'rb') as pdfFileObject:
     pdfReader = PyPDF2.PdfReader(pdfFileObject)
-    pageObj = pdfReader.pages[0ą]
-    print(pageObj.extract_text())
+    page_len = len(pdfReader.pages)
+
+    #extracting text from all pages
+    for page_nr in range(15, page_len):
+        page = pdfReader.pages[page_nr]
+        print(page.extract_text())
