fbb0f01599a82c9829adfa95a8977fde3e180475
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-08-08 20:17:45 +0200

    remove unused parts, simplified code. Put default values

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ file_path = r"C:\Users\zbign_x5x2ftd\OneDrive\Dokumenty\Overcoming_the_Five_Dysf
 
 
 reader = PyPDF2.PdfReader(file_path)
-book_length = len(reader.pages)
-
-
 book_parts = []
 
 
@@ -15,26 +12,6 @@ def visitor_body(text, cm, tm, fontDict, fontSize):
     y = tm[5]
     if 0 < y < 590:
         book_parts.append(text)
-
-for page in range(22, 28):
-    page_nr = reader.pages[page]
-    text_page = page_nr.extract_text(visitor_text=visitor_body)
-book_text_combined = "".join(book_parts)
-
-
-# def get_text(path_to_file):
-#     with open(path_to_file, 'rb') as pdfFileObject:
-#         reader = PyPDF2.PdfReader(pdfFileObject)
-#         book_length = len(reader.pages)
-#
-#         # extracting text from all pages
-#         book_text = []
-#         for page_nr in range(82, 83):
-#             page = reader.pages[page_nr]
-#             text_page = page.extract_text()
-#             book_text.append(text_page)
-#         book_text_combined = "".join(book_text)
-#         return book_text_combined
 
 
 def correct_words(text):
@@ -51,11 +28,16 @@ def correct_words(text):
                       replace(" ’ll", "’ll").
                       replace(" ’d", "’d")
                       )
-    print(corrected_text)
     return corrected_text
+
+
+for page in range(0, len(reader.pages)):
+    page_nr = reader.pages[page]
+    text_page = page_nr.extract_text(visitor_text=visitor_body)
+book_text_combined = "".join(book_parts)
 
 
 language = 'en'
 my_mp3 = gTTS(text=correct_words(book_text_combined), lang=language, slow=False)
 
-my_mp3.save("audiobook_2-compare.mp3")
+my_mp3.save("audiobook.mp3")
