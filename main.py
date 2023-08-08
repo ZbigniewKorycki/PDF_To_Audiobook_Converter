56507592717feb0f284fb1a22d06993b9568dd59
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-08-08 13:31:15 +0200

    modify structure - combine code to get_text function

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import PyPDF2
 from gtts import gTTS
-import os
 
 file_path = r"C:\Users\zbign_x5x2ftd\OneDrive\Dokumenty\Overcoming_the_Five_Dysfunction.pdf"
 
-with open(file_path, 'rb') as pdfFileObject:
-    pdfReader = PyPDF2.PdfReader(pdfFileObject)
-    page_len = len(pdfReader.pages)
 
-    audiobook = []
-    #extracting text from all pages
-    for page_nr in range(10, 20):
-        page = pdfReader.pages[page_nr]
-        text_page = page.extract_text()
-        audiobook.append(text_page)
+def get_text(path_to_file):
+    with open(path_to_file, 'rb') as pdfFileObject:
+        reader = PyPDF2.PdfReader(pdfFileObject)
+        book_length = len(reader.pages)
+
+        # extracting text from all pages
+        book_text = []
+        for page_nr in range(0, book_length):
+            page = reader.pages[page_nr]
+            text_page = page.extract_text()
+            book_text.append(text_page)
+        book_text_combined = " ".join(map(str, book_text))
+        return book_text_combined
 
 
-text_audiobook = " ".join(map(str, audiobook))
-print(text_audiobook)
 language = 'en'
-
-
-my_mp3 = gTTS(text=text_audiobook, lang=language, slow=False)
+my_mp3 = gTTS(text=get_text(file_path), lang=language, slow=False)
 
 my_mp3.save("audiobook_1.mp3")
 
